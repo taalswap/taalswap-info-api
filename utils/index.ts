bb98bf2f09a96074274a6da201ca1c1374207459
Peter Nahm <hwnahm@gmail.com>
2021-05-20 19:04:22 +0900

    Add debugging

--- utils/index.ts
+++ utils/index.ts
@@ -43,7 +43,6 @@ export async function getTokenByAddress(address: string): Promise<Token> {
 
 export async function getTopPairs(): Promise<MappedDetailedPair[]> {
   const epochSecond = Math.round(new Date().getTime() / 1000);
-  console.log(epochSecond - 86400);
   const firstBlock = await getBlockFromTimestamp(epochSecond - 8640000);
 
   if (!firstBlock) {
