68461861f44794d839180f0666a2b38c0c6827f0
Peter H. Nahm <hwnahm@gmail.com>
2024-07-30 14:35:52 +0900

    Change thegraph links

--- utils/apollo/client.ts
+++ utils/apollo/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const client = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/taalswap/exchange"
+    uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.SUBGRAPH_KEY}/subgraphs/id/7T2Z8RmQ4tkAeZtg9rmMHZpTNSXhPjX8k7vZfS5PY8vT`
   }),
   cache: new InMemoryCache()
 });
--- utils/blocks/client.ts
+++ utils/blocks/client.ts
@@ -4,7 +4,7 @@ import fetch from "cross-fetch";
 export const blockClient = new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: "https://api.thegraph.com/subgraphs/name/taalswap/blocks"
+    uri: `https://gateway-arbitrum.network.thegraph.com/api/${process.env.SUBGRAPH_KEY}/subgraphs/id/HeBTP8LPmBiW8AQjuJ1fNNoNHfHji1V38Dfxkhsufoq`
   }),
   cache: new InMemoryCache()
 });
